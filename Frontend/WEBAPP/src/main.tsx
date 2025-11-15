7ab8803c9e0375727c1682a38d2088c794959e40
hyper-kim <kjy6624kss@gmail.com>
2025-11-15 11:26:48 +0000

    front

--- Frontend/WEBAPP/src/main.tsx
+++ Frontend/WEBAPP/src/main.tsx
@@ -1,7 +1,12 @@
 
+  import React from 'react';
   import { createRoot } from "react-dom/client";
   import App from "./App.tsx";
   import "./index.css";
-
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+  import { BrowserRouter } from 'react-router-dom'; // [!!!] 1. 임포트
+  createRoot(document.getElementById("root")!).render(<React.StrictMode>
+    <BrowserRouter> {/* [!!!] 2. <App />을 감싸줍니다 */}
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
