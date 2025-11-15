34ffb318dcc3b895fb27301d905b105515742bb2
hyper-kim <kjy6624kss@gmail.com>
2025-11-15 09:54:01 +0000

    대공사

--- Backend/lab/api_urls.py
+++ Backend/lab/api_urls.py
@@ -1,12 +1,12 @@
 # lab/api_urls.py (새 파일)
 from django.urls import path
-from . import views # 방금 만든 api_views 임포트
+from . import api_views # 방금 만든 api_views 임포트
 
 urlpatterns = [
     # /api/lab/my-profile/ 주소를 LabProfileDetailAPI와 연결
     path(
         'my-profile/', 
-        views.LabProfileDetailAPI.as_view(), 
+        api_views.LabProfileDetailAPI.as_view(), 
         name='api_lab_profile_detail'
     ),
     
