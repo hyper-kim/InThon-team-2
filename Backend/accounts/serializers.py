4978ad37e2af2d741d41f6870d4a56191ee4ea81
twuel0921 <142955347+twuel0921@users.noreply.github.com>
2025-11-15 17:18:28 +0000

    serializers 수정

--- Backend/accounts/serializers.py
+++ Backend/accounts/serializers.py
@@ -5,11 +5,10 @@ class RegisterSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(required=True)
     password1 = serializers.CharField(write_only=True, required=True)
     password2 = serializers.CharField(write_only=True, required=True)
-    nickname = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2', 'nickname']
+        fields = ['email', 'password1', 'password2']
     
     def validate(self, data):
         if data['password1'] != data['password2']:
@@ -21,9 +20,8 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User.objects.create_user(
-            username=validated_data['username'],
+            username=validated_data['email'],
             email=validated_data['email'],
-            password=validated_data['password1'],
-            nickname=validated_data['nickname'],
+            password=validated_data['password1']
         )
         return user
